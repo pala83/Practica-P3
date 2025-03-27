d5f09b97376f1201a3dd4f99ea2b38bf968ac300
Ulises <ulipalazzoo@gmail.com>
2025-03-27 11:50:20 -0300

    Ej1 TP2

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import TP1.TDA.List.LinkedList.LinkedList;
 
 public class App {
@@ -14,5 +16,14 @@ public class App {
         for(Integer elem: list){
             System.out.println(elem);
         }
+    }
+
+    public boolean checkOrder(ArrayList<Integer> arr, int left, int right){
+        if(arr.get(left)>arr.get(right)){
+            return false;
+        }
+        if(left>=right)
+            return true;
+        return checkOrder(arr, left+1, right-1);
     }
 }
